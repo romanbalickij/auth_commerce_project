c568f50da80e5ab4e3165c3bba4bced66097fa35
roman <romanbalitskij9@gmail.com>
2019-06-23 23:47:29 +0300

    fix coupon

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Coupon extends Model
 {
 
+    protected  $fillable = ['code', 'type', 'percent_off'];
+
     public  static function  findByCoupon($code)
     {
         return self:: where('code', $code)->first();
